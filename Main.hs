83940a34311225940a23e803e27e647e712015fe
Jens Petersen <petersen@redhat.com>
2020-06-11 12:48:46 +0800

    new: bump stack to lts-15

--- Main.hs
+++ Main.hs
@@ -224,7 +224,7 @@ newCmd mproject = do
   mstack <- findExecutable "stack"
   -- FIXME add stack.yaml template too
   when (not haveStackCfg && isJust mstack) $ do
-    cmd_ "stack" ["init", "--verbosity", "warn", "--resolver", "lts-14"]
+    cmd_ "stack" ["init", "--verbosity", "warn", "--resolver", "lts-15"]
     sed ["/^#/d", "/^$/d"] "stack.yaml"
   haveGit <- doesDirectoryExist ".git"
   unless haveGit $ do
